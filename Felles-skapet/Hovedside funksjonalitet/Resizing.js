ea5933faafae73939e47bd392f5e5316238bb998
ToBeAss <tobiasmol@uia.no>
2022-12-12 17:02:00 +0100

    Ryddet i index fila og dets tilhørende scripts

--- Felles-skapet/Hovedside funksjonalitet/Resizing.js
+++ Felles-skapet/Hovedside funksjonalitet/Resizing.js
@@ -1,42 +1,34 @@
-// WINDOW RESIZING - HEADER CSS & PAGE CONTENT CSS
-// STATUS: DONE
-// NEED HELP: -
-/* COMMENT: Seksjonen tilpasser utseendet til "header" slik at den alltid er synlig, selv om brukeren endrer størrelsen på skjermen.
+/*
+    DETTE SCRIPTET TILPASSER INNHOLDET PÅ SIDEN SLIK AT DET ALLTID ER SYNLIG,
+    SELV OM BRUKEREN ENDRER STØRRELSEN PÅ SKJERMVINDUET.
 */
 
-    // Lager en tom variabel som brukes senere
-    var headerHight;
-
-    // Starter variabelnavnet med HTML for å vise at det er et HTML-element
-    var HTMLheader = document.getElementById("header"); 
-    var HTMLpageName = document.getElementById("pageName");
-    var HTMLlogo = document.getElementById("logo");
-    var HTMLgroupName = document.getElementById("groupName");
-    var HTMLnavigationMenu = document.getElementById("navigationMenu");
+    // HENTER INN HTML-ELEMENTENE SOM BLIR TATT I BRUK
+    // STARTER VARIABELNAVNENE MED "HTML" FOR Å ILLUSTRERE AT DET ER HTML-ELEMENTER
+    var HTMLheader = document.getElementById("header");
+    var HTMLlogo = document.getElementById("header__logo");
+    var HTMLtitle = document.getElementById("header__title");
+    var HTMLgroupName = document.getElementById("header__group-name");
+    var HTMLnavigationMenu = document.getElementById("header__navigation-menu");
     var HTMLpage = document.getElementById("page");
+    var pageContents = document.getElementById("page").children; // Henter inn iframe-elementene som et array (med 4 elementer)
 
-    // Henter inn iframe-elementene med ClassName - det blir et array med 4 elementer
-    var HTMLPageContents = document.getElementsByClassName("PageContents");
+    var headerHight; // Lager en tom variabel som brukes til å bestemme høyden til "header", og deretter høyden til "page"
 
-    // Sjekker når størrelsen på vinduet blir endra og kjører funksjonen "ResizeHeader" og "ResizePage"
-    window.onresize = function() {ResizeHeader(); ResizePage();}
-
-    // Funksjonen inneholder 4 forskjellige stadier en skjerm kan ha (bredde) og endrer de ulike elementene i "header" ved hjelp av CSS, slik at de alltid er synlige
-    function ResizeHeader ()
+    // DENNE FUNKSJONEN VELGER MELLOM 3 FORSKJELLIGE STADIER ER SKJERM KAN HA (BREDDE) OG ENDRER DE ULIKE ELEMENTENE I "HEADER" VED HJELP AV CSS
+    function resizeHeader ()
     {
-        if (window.innerWidth < 560)
+        if (window.innerWidth < 560) // window.innerWidth er en innebygd variabel som måler bredden på skjermen
         {
             headerHight = 125;
-            HTMLheader.style.height = headerHight + "px";
 
-            HTMLpageName.style.marginTop = "80px";
-            HTMLpageName.style.position = "relative";
-            HTMLpageName.style.textAlign = "center";
-            HTMLpageName.style.marginRight = "0px";
+            HTMLtitle.style.marginTop = "80px";
+            HTMLtitle.style.position = "relative";
+            HTMLtitle.style.textAlign = "center";
+            HTMLtitle.style.marginRight = "0px";
 
             HTMLlogo.style.marginLeft = "0px";
 
-            HTMLgroupName.hidden = false;
             HTMLgroupName.style.marginLeft = "195px";
 
             HTMLnavigationMenu.style.marginTop = "125px";
@@ -48,16 +40,14 @@
         else if (window.innerWidth < 1175)
         {
             headerHight = 150;
-            HTMLheader.style.height = headerHight + "px";
 
-            HTMLpageName.style.marginTop = "35px";
-            HTMLpageName.style.position = "absolute";
-            HTMLpageName.style.right = "0";
-            HTMLpageName.style.marginRight = "25px";
+            HTMLtitle.style.marginTop = "35px";
+            HTMLtitle.style.position = "absolute";
+            HTMLtitle.style.right = "0";
+            HTMLtitle.style.marginRight = "25px";
 
             HTMLlogo.style.marginLeft = "25px";
 
-            HTMLgroupName.hidden = false;
             HTMLgroupName.style.marginLeft = "225px";
 
             HTMLnavigationMenu.style.marginTop = "100px";
@@ -66,19 +56,17 @@
 
             HTMLpage.style.marginTop = "150px";
         }
-        else
+        else // Om skjermen er bredere enn 1175
         {
             headerHight = 100;
-            HTMLheader.style.height = headerHight + "px";
 
-            HTMLpageName.style.marginTop = "35px";
-            HTMLpageName.style.position = "relative";
-            HTMLpageName.style.textAlign = "center";
-            HTMLpageName.style.marginRight = "0px";
+            HTMLtitle.style.marginTop = "35px";
+            HTMLtitle.style.position = "relative";
+            HTMLtitle.style.textAlign = "center";
+            HTMLtitle.style.marginRight = "0px";
 
             HTMLlogo.style.marginLeft = "25px";
 
-            HTMLgroupName.hidden = false;
             HTMLgroupName.style.marginLeft = "225px";
 
             HTMLnavigationMenu.style.marginTop = "40px";
@@ -87,17 +75,22 @@
 
             HTMLpage.style.marginTop = "100px";
         }
+
+        HTMLheader.style.height = headerHight + "px"; // Endrer høyden på "header" basert på den oppdaterte verdien av variabelen "headerHight" fra "if"-setningene
     }
 
-    // Funksjonen kjører gjennom arrayet HTMLPageContents og endrer høydem på de 4 elementene til høyden på vinduet - høyden på header-en
-    function ResizePage ()
+    // DENNE FUNKSJONEN GÅR GJENNOM ARRAYET "pageContents" OG ENDRER HØYDEN PÅ ALLE DE ELEMENTENE (4) TIL HØYDEN AV VINDUET MINUS (-) HØYDEN TIL "HEADER"
+    function resizePage ()
     {
-        for (let i = 0; i < HTMLPageContents.length; i++)
+        for (let i = 0; i < pageContents.length; i++)
         {
-            HTMLPageContents[i].style.height = window.innerHeight-headerHight + "px";
+            pageContents[i].style.height = window.innerHeight-headerHight + "px";
         }
     }
 
-    // Kaller funksjonene ved oppstart
-    ResizeHeader();
-    ResizePage();
+    // DENNE FUNKSJONEN SJEKKER KONSTANT NÅR STØRRELSEN PÅ VINDUET BLIR ENDRET PÅ OG KJØRER DERETTER FUNKSJONENE "resizeHeader" OG "resizePage"
+    window.onresize = function() {resizeHeader(); resizePage();}
+
+    // KALLER BEGGE FUNKSJONENE VED OPPSTART SLIK AT SIDESTØRRELSEN ALLTID ER OPPDATERT
+    resizeHeader();
+    resizePage();
